730baa7f1157e002239729f07938d6c1485fc6d5
Raul Rodriguez <raul.rodriguez.luna@gmail.com>
2016-11-30 22:21:48 -0500

    Commit changes for Brunner exercie

---
+++ Exercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static double Checkout(string[] items, bool discount)
+        {
+            if (items.Length == 0)
+                return 0.0;
+
+            var applesCounter = items.Count(x => "apple".Equals(x, StringComparison.InvariantCultureIgnoreCase));
+            var orangesCounter = items.Length - applesCounter;
+
+            if(!discount)
+                return (applesCounter * 0.65) + (orangesCounter * 0.45);
+
+            applesCounter = applesCounter / 2;
+
+            if(orangesCounter % 3 == 0)
+            {
+                var extras = orangesCounter / 3;
+                orangesCounter = extras * 2;
+            }
+
+            return (applesCounter * 0.65) + (orangesCounter * 0.45);
+        }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        //public void VerifyCheckoutIsCorrect()
+        //{
+        //    var result = Program.Checkout(new[] { "Orange", "Apple" });
+        //    Assert.AreEqual(1.1, result);
+        //}
+
+        //[TestMethod]
+        //public void VerifyCheckoutIsZero()
+        //{
+        //    var result = Program.Checkout(new List<string>().ToArray());
+        //    Assert.AreEqual(0.0, result);
+        //}
+
+        //[TestMethod]
+        //public void VerifyCheckoutIsZeroWithUnknownItems()
+        //{
+        //    var result = Program.Checkout(new[] { "Banana" });
+        //    Assert.AreNotEqual(0.0, result);
+        //}
+
+        [TestMethod]
+        public void VerifyCheckoutWithDiscount()
+        {
+            var result = Program.Checkout(new[] 
+            { 
+                "Orange", 
+                "Orange",
+                "Orange",
+                "Apple",
+                "Apple"
+            }, true);
+            Assert.AreEqual(result, 1.55);
+        }
+    }
+}
